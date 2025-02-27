38f2f76082856e43a0ec9a7b83b68bfc97c63514
Gustavo Ávila <71139006+gusavila@users.noreply.github.com>
2025-02-27 16:47:15 -0300

    Create home page and server

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
